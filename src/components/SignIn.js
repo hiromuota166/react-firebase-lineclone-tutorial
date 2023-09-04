859a47226ee03f40bd677203c21d1e43018cf212
hiromuota166 <otahiromu166@gmail.com>
2023-09-05 00:44:31 +0900

    設定完了　本題へ

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,12 +1,13 @@
 // "rfce"で最初の雛形が生成できる
 
-import { Button } from '@mui/material'
-import React from 'react'
+import React from 'react';
 import firebase from "firebase/compat/app"; 
 import { auth } from "../firebase.js";
+import Button from '@mui/material/Button';
 
 function SignIn() {
   function signInWithGoogle() {
+    //googleに簡単に入れる関数
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
@@ -17,4 +18,7 @@ function SignIn() {
   );
 }
 
-export default SignIn
+export default SignIn;
+
+//ユーズオースステート(??)firebaseのフックスが用意している
+//ログイン状態をtrue or falseで確認できる
---
+++ src/components/SignOut.js
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material';
+import { auth } from '../firebase.js';
+import React from 'react';
+
+function SignOut() {
+  return (
+    <div>
+      <Button onClick={() => auth.signOut()}>
+        サインアウト
+      </Button>
+    </div>
+  )
+}
+
+export default SignOut;
